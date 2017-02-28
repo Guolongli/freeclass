869edc4998a91190310ee0c2a09c2f516274545c
Guolongli <2675290957@qq.com>
2017-02-28 16:13:23 +0800

    Signed-off-by: Guolongli <2675290957@qq.com>

--- freeclass/Application/Common/Conf/config.php
+++ freeclass/Application/Common/Conf/config.php
@@ -2,10 +2,10 @@
 return array(
 	//'配置项'=>'配置值'
 		'DB_TYPE'=> 'mysql',
-		'DB_HOST'=> 'localhost',
-		'DB_NAME'=>'freeclass',
-		'DB_USER'=>'root',
-		'DB_PWD'=>'baolilong',
+		'DB_HOST'=> '',//数据库主机
+		'DB_NAME'=>'',//数据库名
+		'DB_USER'=>'',//数据库用户名
+		'DB_PWD'=>'',//数据库密码
 		'DB_PORT'=>'3306',
 	// 显示页面Trace信息
 		'SHOW_PAGE_TRACE' =>false,
